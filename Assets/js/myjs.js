169cedbc4e12f9e1bdbbd8c417cca30631764e00
A Yon 4th <156146835+Sciberius1@users.noreply.github.com>
2024-06-11 07:43:42 -0700

    working on mime type error

--- Assets/js/myjs.js
+++
@@ -1,5 +0,0 @@
-// Adjusts the selection of the navbar targets for the floating header //
-
-window.addEventListener("hashchange", function() {
-    window.scrollTo(window.scrollX, window.scrollY - 75);
-});

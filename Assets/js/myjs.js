8d3dd733fa08cd30df23859e2989fd9ced3ec342
A Yon 4th <156146835+Sciberius1@users.noreply.github.com>
2024-06-11 18:04:45 -0700

    removing java errors

--- Assets/js/myjs.js
+++
@@ -1,6 +0,0 @@
-// This is to prevent the header from covering the section titles when clicked //
-document.querySelectorAll('.navbar-item').forEach(function(item) {
-    item.addEventListener('click', function() {
-        window.scrollTo(window.scrollX, window.scrollY - 75);
-    });
-});
